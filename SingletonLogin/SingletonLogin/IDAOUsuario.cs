e80f91ffd704288b1f71822b596b5f982a2b36a0
Ricardo <ricardoananiasjr@icloud.com>
2019-04-26 17:03:04 -0300

    Init Commit

---
+++ SingletonLogin/SingletonLogin/IDAOUsuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonLogin
+{
+    interface IDAOUsuario
+    {
+        List<Usuario> GetAll();
+        Usuario Getusuario(string login, int senha);
+        void Update();
+
+
+
+
+    }
+}
---
+++ SingletonLogin/SingletonLogin/Usuario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonLogin
+{
+    class Usuario
+    {
+        private string _nome;
+        private int _cpf;
+        private string _login;
+        private int _senha;
+
+        public static Usuario _instance;
+        
+        private Usuario() { }
+
+        public string nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public int cpf
+        {
+            get { return _cpf; }
+            set { _cpf = value; }
+        }
+
+        public string login
+        {
+            get { return _login; }
+            set { _login = value; }
+        }
+
+        public int senha
+        {
+            get { return _senha; }
+            set { _senha = value; }
+        }
+
+        
+
+
+
+
+
+
+
+
+    }
+}
